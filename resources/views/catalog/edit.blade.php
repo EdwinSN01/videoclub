6102d64c026dec1fbb01705c5148faded7f169dd
EdwinSN01 <edserin@unitru.edu.pe>
2024-05-24 23:02:17 -0500

    videoclub 1

---
+++ resources/views/catalog/edit.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+        Modificar pelicula {{$id}}
+@stop
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+        Pantalla principal
+@stop
